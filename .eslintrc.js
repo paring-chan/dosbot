43df9facd39e2c8ae232f3986218776902730dfa
PIKOSTUDIO <parangee9706@gmail.com>
2020-08-30 18:04:02 +0900

    [수정] eslint 규칙 수정

--- .eslintrc.js
+++ .eslintrc.js
@@ -35,6 +35,7 @@ module.exports = {
         '@typescript-eslint/no-explicit-any': 0,
         '@typescript-eslint/no-namespace': 0,
         '@typescript-eslint/ban-ts-comment': 0,
-        '@typescript-eslint/no-unused-vars': 0
+        '@typescript-eslint/no-unused-vars': 0,
+        '@typescript-eslint/explicit-module-boundary-types': 0
     }
 }
