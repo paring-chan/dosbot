92e59f8de04fb8485266f6d356850f2ddc34a9e2
piko <parangee9706@gmail.com>
2020-09-06 22:15:20 +0900

    [커밋] 입장메시지 추가

--- src/listeners/jqmsg.ts
+++ src/listeners/jqmsg.ts
@@ -4,9 +4,58 @@ import {Guild} from '../models'
 export async function join(member: GuildMember) {
     const gd = (await Guild.findOne({id: member.guild.id}))!
     if (!gd.inMsg || gd.inMsg === '') return
-    const c = <TextChannel|undefined>member.guild.channels.cache.filter(r => r.type === 'text').get(gd.inChn || '')
+    const c = <TextChannel|undefined>member.guild.channels.cache.filter(r => r.type === 'text').find(r => r.id === (gd.inChn ? gd.inChn : ''))
+
+    let res = gd.inMsg
+
+    const placeholders = [{
+        name: '@member',
+        to: member.toString()
+    }, {
+        name: 'member',
+        to: member.user.tag
+    }, {
+        name: 'guild',
+        to: member.guild.name
+    }, {
+        name: 'guild.members',
+        to: member.guild.memberCount
+    }]
+
+    // @ts-ignore
+    placeholders.forEach((placeholder: {name: string, to: string}) => {
+        res = res.replace(`{${placeholder.name}}`, placeholder.to)
+    })
 
     if (!c) return
 
-    await c.send(gd.inMsg)
+    await c.send(res)
+}
+
+export async function leave(member: GuildMember) {
+    const gd = (await Guild.findOne({id: member.guild.id}))!
+    if (!gd.outMsg || gd.outMsg === '') return
+    const c = <TextChannel|undefined>member.guild.channels.cache.filter(r => r.type === 'text').find(r => r.id === (gd.outChn ? gd.outChn : ''))
+
+    let res = gd.outMsg
+
+    const placeholders = [{
+        name: 'member',
+        to: member.user.tag
+    }, {
+        name: 'guild',
+        to: member.guild.name
+    }, {
+        name: 'guild.members',
+        to: member.guild.memberCount
+    }]
+
+    // @ts-ignore
+    placeholders.forEach((placeholder: {name: string, to: string}) => {
+        res = res.replace(`{${placeholder.name}}`, placeholder.to)
+    })
+
+    if (!c) return
+
+    await c.send(res)
 }
